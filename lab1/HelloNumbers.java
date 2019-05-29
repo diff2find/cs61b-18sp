a21697398eaaa177e0ff86e4953c47339a66745d
diff2find <454473316@qq.com>
2019-05-29 14:54:15 +0800

    added HelloNumbers.java fixed file of zero

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -3,9 +3,10 @@ public class HelloNumbers {
         int x = 0;
         int i = 0;
         while (i < 10) {
-            System.out.print(x + " ");
+            
             x = x + i;
             i = i + 1;
+            System.out.print(x + " ");
         }
     }
 }
